b6a619fcee95279ec07c079bc8b48d8e22ebe073
JLRC <tangun73@gmail.com>
2020-05-17 16:11:03 +0200

    added perimeter

--- recursion_pattern_matching.erl
+++ recursion_pattern_matching.erl
@@ -1,10 +1,20 @@
 -module(recursion_pattern_matching).
--export([bits/1, perimeter/1]).
+-export([bits/1, perimeter/1, listSum/1]).
+
+listSum(L) -> 
+    listSum(L, 0).
+
+listSum([H|T], S) ->
+    listSum(T, H+S);
+
+listSum([], S) ->
+    S.
+
 
 bits(0, P) ->
     P;
 
-
+% divide number by 2 and sum of the remainder of the division
 bits(N, P) ->
     bits(trunc(N/2), P + (trunc(N) rem 2)).
 
@@ -13,5 +23,10 @@ bits(N) ->
     bits(N, 0).
 
 
+%Not sure about the first assignment.
 perimeter({triangle, {A, B, C}}) ->
-    A + B + C.
+    A + B + C;
+
+%perimeter of shape is sum of all the sides
+perimeter({shape, L}) -> 
+    listSum(L).
